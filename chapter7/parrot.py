3e7cbc87dd4a9598b32c7456f974b874aba1dffc
bubblelone@163.com <bubblelone@163.com>
2019-01-27 19:40:25 +0800

    修改

--- chapter7/parrot.py
+++ chapter7/parrot.py
@@ -1,9 +1,13 @@
 
 promt = "\ntell me something,and i will repeat it back to you:"
 promt += "\nEnter 'quit' to end the program."
-message = ""
-while message != 'quit':
+
+active = True
+
+while active :
     message = input(promt)
 
-    if message != 'quit':
+    if message == 'quit':
+        active = False
+    else:
         print(message)
