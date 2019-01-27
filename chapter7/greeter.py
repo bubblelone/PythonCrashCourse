81d918c4a58b5b37b0999dd3e4f3bb572e1741a3
bubblelone@163.com <bubblelone@163.com>
2019-01-27 17:01:42 +0800

    Initial commit

---
+++ chapter7/greeter.py
@@ -0,0 +1,7 @@
+
+
+prompt = "if you tell us who you are ,we can personalize the message you see. "
+prompt +=  "\nwhat is your first name?"
+
+name  = input(prompt)
+print("\nhello, " + name + "!")
---
+++ chapter7/rollercoaster.py
@@ -0,0 +1,9 @@
+
+
+height = input("how tall are you, in inches?")
+height = int(height)
+
+if height >= 36:
+    print("\nyou're tall enough to ride!")
+else:
+    print("\nyou'll be able to ride when you're a little older.")
