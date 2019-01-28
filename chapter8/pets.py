dee885e7135a2c35f39327502ba55a18b9930f58
bubblelone <bubblelone@163.com>
2019-01-28 10:40:34 +0800

    修改

---
+++ chapter8/pets.py
@@ -0,0 +1,6 @@
+
+def describe_pet(animal_type, pet_name):
+    print("\ni have a " + animal_type + ".")
+    print("my " + animal_type + "'s name is " + pet_name.title()+ ".")
+
+describe_pet('hamster', 'harry')
