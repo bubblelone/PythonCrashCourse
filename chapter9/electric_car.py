75ca74de0232b35c73e074159756fa4c27979946
bubblelone@163.com <bubblelone@163.com>
2019-01-28 21:01:50 +0800

    修改

--- chapter9/electric_car.py
+++ chapter9/electric_car.py
@@ -22,9 +22,31 @@ class Car():
     def increment_odometer(self, miles):
         self.odometer_reading += miles
 
+class Battery():
+    def __init__(self, battery_size=70):
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        print("this car has a " + str(self.battery_size) + "-kwh battery.")
+
+    def get_range(self):
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+        message = "this car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
+
+
 class ElectricCar(Car):
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
+        self.battery = Battery(85)
+
+
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
