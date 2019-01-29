9f91bed0f4249d2a37e08e5de75ceeb41a572a36
bubblelone <bubblelone@163.com>
2019-01-29 14:52:33 +0800

    修改

---
+++ chapter10/division.py
@@ -0,0 +1,13 @@
+print("give me two numbers , and i'll divide them.")
+print("enter 'q' to quit.")
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input("Second number: ")
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print("you can't divide by 0!")
+    else:
+        print(answer)
