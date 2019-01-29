ace8a21198bf0d6f1efef0bd9bf81653759568cf
bubblelone@163.com <bubblelone@163.com>
2019-01-29 19:54:12 +0800

    修改

--- chapter11/test_survey.py
+++ chapter11/test_survey.py
@@ -4,24 +4,21 @@ from survey import AnonymousSurvey
 class TestAnonymousSurvey(unittest.TestCase):
     def setUp(self):
         question = "what language did you first learn to speak?"
-        my_surevy = AnonymousSurvey(question)
-
+        self.my_surevy = AnonymousSurvey(question)
+        self.responses = ['english', 'spanish', 'mandarin']
 
 
     def test_store_single_response(self):
-        question = "what language did you first learn to speak?"
-        my_surevy = AnonymousSurvey(question)
-        my_surevy.store_response('english')
-        self.assertIn('english', my_surevy.responses)
+
+        self.my_surevy.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.my_surevy.responses)
 
     def test_store_three_responses(self):
-        question = "what language did you first learn to speak?"
-        my_surevy = AnonymousSurvey(question)
-        responses = ['english','spanish','mandarin']
-        for response in responses:
-            my_surevy.store_response(response)
-        for response in responses:
-            self.assertIn(response, my_surevy.responses)
+
+        for response in self.responses:
+            self.my_surevy.store_response(response)
+        for response in self.responses:
+            self.assertIn(response, self.my_surevy.responses)
 
 
 if __name__ == '__main__':
